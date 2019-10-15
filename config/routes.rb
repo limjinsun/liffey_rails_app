c7d61597c9c7381f8deec76c7e04c9c5890a45b4
limjinsun <limjinsun@limjinsuns-MacBook-Pro.local>
2019-10-15 20:56:18 +0100

    💻 Devise login funtion added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { 
+  	:sessions => 'customdevise/sessions',
+  	:passwords => 'customdevise/passwords',
+  	:registrations => 'customdevise/registrations' }  # generate all route for devise - with user object.
+  
   get 'home/index' # without this line - http get request -'home/index' won't work.
-  resources :posts # without this line - all http request railted to 'posts' won't work.
+  resources :posts # generate all route for posts.
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root 'posts#index' # This will root address to re direct to posts 'index' actions.
