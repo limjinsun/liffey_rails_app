9b94d32815aa158c51f065598c69383aa17cb39e
Jin Lim <limjinsun@gmail.com>
2019-11-29 00:02:25 +0000

    asset-compile-fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # config.assets.compile = false
+  config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -125,7 +125,6 @@ Rails.application.configure do
 
   config.require_master_key = false
 
-  config.assets.compile = true #set to false by default
   config.assets.digest = true
 
 end
