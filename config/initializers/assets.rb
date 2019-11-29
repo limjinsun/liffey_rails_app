b219046cf5fc165822aa923198a2ccc67ad50a1a
Jin Lim <limjinsun@gmail.com>
2019-11-29 03:39:15 +0000

    asset-url-fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,3 +16,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w[ckeditor/config.js]
 Rails.application.config.assets.precompile += %w( ckeditor/content-style.css )
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
